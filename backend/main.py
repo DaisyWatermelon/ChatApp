8b56185524ca0deeb2db5f23053651c78eb689ce
n10362428 <n10362428@QUT-PA00152680>
2024-06-17 23:04:51 +1000

    backend

---
+++ backend/main.py
@@ -0,0 +1,32 @@
+# from fastapi import FastAPI
+import fastapi
+from fastapi.middleware.cors import CORSMiddleware
+
+from pydantic import BaseModel
+
+import requests
+
+PRIVATE_KEY = "dfd262e0-8a80-4575-a1a8-5957641cd9e2"
+app = fastapi.FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class User(BaseModel):
+    username: str
+
+@app.post('/authenticate')
+async def authenticate(user: User):
+    response = requests.put('https://api.chatengine.io/users/',
+        data={
+            "username": user.username,
+            "secret": user.username,
+            "first_name": user.username,
+        },
+        headers={ "Private-Key": PRIVATE_KEY }
+    )
+    return response.json()
